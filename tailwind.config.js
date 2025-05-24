032592b319e4792be2db568ed8c90d0d179f13a4
daqyasarkocyigit <yasarkocyigit@daqconsulting.com>
2025-05-23 16:48:48 -1000

    Enhance visual design and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,42 +40,6 @@ export default {
           800: '#2d3441',
           900: '#1b1f28',
         },
-        'electric-blue': {
-          50: '#f0f9ff',
-          100: '#e0f2fe',
-          200: '#b9e6fe',
-          300: '#7cd4fd',
-          400: '#36bffa',
-          500: '#0ba5ec',
-          600: '#0284c7',
-          700: '#036ba1',
-          800: '#075985',
-          900: '#0c4a6e',
-        },
-        'midnight-purple': {
-          50: '#faf5ff',
-          100: '#f3e8ff',
-          200: '#e9d5ff',
-          300: '#d8b4fe',
-          400: '#c084fc',
-          500: '#a855f7',
-          600: '#9333ea',
-          700: '#7e22ce',
-          800: '#6b21a8',
-          900: '#581c87',
-        },
-        'neon-orange': {
-          50: '#fff7ed',
-          100: '#ffedd5',
-          200: '#fed7aa',
-          300: '#fdba74',
-          400: '#fb923c',
-          500: '#f97316',
-          600: '#ea580c',
-          700: '#c2410c',
-          800: '#9a3412',
-          900: '#7c2d12',
-        }
       },
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
@@ -93,29 +57,6 @@ export default {
       backgroundImage: {
         'hero-pattern': "url('https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1600')",
         'cta-pattern': "url('https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg?auto=compress&cs=tinysrgb&w=1600')",
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            maxWidth: '65ch',
-            color: '#1b1f28',
-            p: {
-              lineHeight: '1.75',
-            },
-            h1: {
-              fontWeight: '700',
-              lineHeight: '1.25',
-            },
-            h2: {
-              fontWeight: '600',
-              lineHeight: '1.35',
-            },
-            h3: {
-              fontWeight: '600',
-              lineHeight: '1.375',
-            },
-          },
-        },
       },
     },
   },
