7b65cee0fef844649d8be687cc31551ad1893f64
daqyasarkocyigit <yasarkocyigit@daqconsulting.com>
2025-05-23 16:56:13 -1000

    Fix Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -61,4 +64,4 @@ export default {
     },
   },
   plugins: [],
-};
+}
