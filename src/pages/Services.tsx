1f6fe257313ea59a01c589731d9bfa6d40efcb1b
daqyasarkocyigit <yasarkocyigit@daqconsulting.com>
2025-05-30 16:32:34 -0400

    Update Data Engineering service image

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -57,7 +57,7 @@ const Services: React.FC = () => {
           "Real-time data processing solutions",
           "Data quality and governance frameworks"
         ]}
-        image="https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        image="https://images.pexels.com/photos/1181354/pexels-photo-1181354.jpeg?auto=compress&cs=tinysrgb&w=1600"
         icon={<Database size={32} />}
       />
 
