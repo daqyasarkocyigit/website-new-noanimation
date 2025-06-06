980310ee6818014f44d978a3241f08417b94ca5c
daqyasarkocyigit <yasarkocyigit@daqconsulting.com>
2025-06-05 22:41:20 -0400

    Deploy application to Netlify

--- src/components/services/ServiceDetail.tsx
+++ src/components/services/ServiceDetail.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { Check } from 'lucide-react';
 import AnimatedSection from '../utils/AnimatedSection';
-import Scene from './3d/Scene';
-import DataEngineeringModel from './3d/DataEngineeringModel';
-import VisualizationModel from './3d/VisualizationModel';
-import CloudModel from './3d/CloudModel';
-import AIModel from './3d/AIModel';
 
 interface ServiceDetailProps {
   id: string;
@@ -23,24 +18,10 @@ const ServiceDetail: React.FC<ServiceDetailProps> = ({
   title,
   description,
   benefits,
+  image,
   icon,
   isReversed = false,
 }) => {
-  const get3DModel = (id: string) => {
-    switch (id) {
-      case 'data-engineering':
-        return <DataEngineeringModel />;
-      case 'data-visualization':
-        return <VisualizationModel />;
-      case 'cloud-modernization':
-        return <CloudModel />;
-      case 'ai-engineering':
-        return <AIModel />;
-      default:
-        return <DataEngineeringModel />;
-    }
-  };
-
   return (
     <section id={id} className="py-20 scroll-mt-24">
       <div className="container mx-auto px-4 md:px-6">
@@ -73,12 +54,14 @@ const ServiceDetail: React.FC<ServiceDetailProps> = ({
           </AnimatedSection>
           
           <AnimatedSection delay={0.3} className={`order-1 ${isReversed ? 'md:order-1' : 'md:order-2'}`}>
-            <div className="relative rounded-xl overflow-hidden shadow-2xl">
+            <div className="relative rounded-xl overflow-hidden shadow-2xl aspect-w-16 aspect-h-9">
               <div className="absolute -top-6 -right-6 w-32 h-32 bg-brand-red-600/10 rounded-full filter blur-3xl"></div>
               <div className="absolute -bottom-6 -left-6 w-32 h-32 bg-gray-400/10 rounded-full filter blur-3xl"></div>
-              <Scene>
-                {get3DModel(id)}
-              </Scene>
+              <img 
+                src={image} 
+                alt={title} 
+                className="w-full h-full object-cover z-10 relative"
+              />
             </div>
           </AnimatedSection>
         </div>
