836efc47e5823ea340459226ab535d98b3cbca07
daqyasarkocyigit <yasarkocyigit@daqconsulting.com>
2025-08-18 16:38:12 -0400

    Remove NewsUpdates Section from Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
-import NewsUpdates from '../components/home/NewsUpdates';
 
 const Home: React.FC = () => {
   return (
     <>
       <Hero />
-      <NewsUpdates />
     </>
   );
 };
