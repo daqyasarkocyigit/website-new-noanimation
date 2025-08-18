837c996b528f57c959fb21e9a2215fb525b88fad
daqyasarkocyigit <yasarkocyigit@daqconsulting.com>
2025-08-18 16:22:55 -0400

    Create Professional Service Visuals

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
-import NewsUpdates from '../components/home/NewsUpdates';
 
 const Home: React.FC = () => {
-  return (
-    <>
-      <Hero />
-      <NewsUpdates />
-    </>
-  );
+  return <Hero />;
 };
 
 export default Home;
