69856bb3c46890e4b9e78498a9044b731d5f5d17
daqyasarkocyigit <yasarkocyigit@daqconsulting.com>
2025-08-18 16:36:01 -0400

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
-import NewsTicker from '../components/home/NewsTicker';
+import NewsUpdates from '../components/home/NewsUpdates';
 
 const Home: React.FC = () => {
   return (
     <>
       <Hero />
-      <NewsTicker />
+      <NewsUpdates />
     </>
   );
 };
