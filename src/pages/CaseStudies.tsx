75fe8dd900adaae949485717c0a14d4032da9d31
daqyasarkocyigit <yasarkocyigit@daqconsulting.com>
2025-09-16 11:57:13 -0400

    Updated CaseStudies.tsx

--- src/pages/CaseStudies.tsx
+++ src/pages/CaseStudies.tsx
@@ -5,6 +5,7 @@ import AnimatedSection from '../components/utils/AnimatedSection';
 import CallToAction from '../components/home/CallToAction';
 
 const CaseStudies: React.FC = () => {
+}
 import React from 'react';
 import { motion } from 'framer-motion';
 import CaseStudyCard from '../components/caseStudies/CaseStudyCard';
