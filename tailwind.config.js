f194443c37d180a839b15859d6525d9a634b3b19
daqyasarkocyigit <yasarkocyigit@daqconsulting.com>
2025-09-17 14:04:37 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,40 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      animation: {
+        blob: 'blob 7s infinite',
+        'blob-reverse': 'blob-reverse 8s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-20px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
+        'blob-reverse': {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(-30px, 50px) scale(0.9)',
+          },
+          '66%': {
+            transform: 'translate(20px, -20px) scale(1.1)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
+      },
       colors: {
         'brand-red': {
           50: '#fff5f5',
